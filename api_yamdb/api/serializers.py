658e5e3e5aa36efc92f36d6db724c266edc45734
Виктор Алтунин <altvik@yandex.ru>
2022-10-06 00:54:30 +0500

    pre-assembly_v1

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 import datetime as dt
+import re
 
-from rest_framework import serializers
+from rest_framework import serializers, validators
 
 from titles.models import User, Category, Genre, Title, Review, Comment
 
@@ -20,6 +21,54 @@ class UserSerializer(serializers.ModelSerializer):
         )
         lookup_field = "username"
         read_only_field = ('role',)
+
+
+class RegistrationSerializer(serializers.Serializer):
+    """Сериализатор данных регистрации."""
+
+    username = serializers.CharField(
+        validators=(
+            validators.UniqueValidator(
+                queryset=User.objects.all(),
+                message='Имя занято.'
+            ),
+        ),
+        required=True,
+    )
+
+    email = serializers.EmailField(
+        validators=(
+            validators.UniqueValidator(
+                queryset=User.objects.all(),
+                message='Email уже зарегистрирован.'
+            ),
+        ),
+        required=True,
+    )
+
+    def validate_username(self, value):
+        """Валидация поля username."""
+
+        if value.lower() == 'me':
+            raise serializers.ValidationError(
+                "Имя зарезервировано."
+            )
+
+        if not re.match(r'^[\w.@+-]+\z', value): # шаблон из задания, может \Z
+        # if not re.match(r'^[\w.@+-]+)$', value): # шаблон из примеров
+        # r'^users/(?P<username>[\w.@+-]+)$' # шаблон для username
+            raise serializers.ValidationError(
+                "Неверный формат имени."
+            )
+
+        return value
+
+
+class AuthetificationSerializer(serializers.Serializer):
+    """Сериализатор данных аутентификации."""
+
+    username = serializers.CharField(required=True)
+    confirmation_code = serializers.CharField(required=True)
 
 
 class CategorySerializer(serializers.ModelSerializer):
